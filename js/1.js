cb36c1a23d7e2a71d55b3e0feccffffe270a09e4
Anna <Anna@gmail.com>
2026-09-23 11:00:21 -0300

    java

---
+++ js/1.js
@@ -0,0 +1,11 @@
+console.log("Olá mundão Perdido!");
+// Declarar variaveis em JS.
+
+i=0; //variavel global var
+var nome = "Anna"; //define variavel global
+nome = 1;
+let numero; //define variavel local por bloco
+const numeroPI =3.141516;
+
+console.log("Variavel i:"+typeof(i));
+console.log("Variavel i:"+typeof(nome));
